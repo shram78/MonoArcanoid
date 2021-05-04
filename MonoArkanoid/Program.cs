3d1e2281bf9786ae11c58d82cb744ada8d8a4cbe
Александр Шрам <ra00275@gmail.com>
2021-05-04 20:16:15 +0300

    Добавьте файлы проекта.

---
+++ MonoArkanoid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MonoArkanoid
+{
+#if WINDOWS || LINUX
+
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MonoArkanoid())
+                game.Run();
+        }
+    }
+#endif
+}
